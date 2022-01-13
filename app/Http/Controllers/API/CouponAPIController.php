b3c3267269144fa50b88bc3b58a31d7e2666411d
Dev112523 <binch991005@gmail.com>
2022-01-13 22:24:42 +0000

    completed

---
+++ app/Http/Controllers/API/CouponAPIController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+
+use App\Criteria\Coupons\ValidCriteria;
+use App\Criteria\Users\FilterByUserCriteria;
+use App\Models\Coupon;
+use App\Models\CouponHistory;
+use App\Notifications\CouponUsed;
+use App\Repositories\CouponRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+use App\Http\Controllers\Controller;
+use InfyOm\Generator\Criteria\LimitOffsetCriteria;
+use Prettus\Repository\Criteria\RequestCriteria;
+use Illuminate\Support\Facades\Response;
+use Prettus\Repository\Exceptions\RepositoryException;
+use Flash;
+use Auth;
+/**
+ * Class CouponController
+ * @package App\Http\Controllers\API
+ */
+
+class CouponAPIController extends Controller
+{
+    /** @var  CouponRepository */
+    private $couponRepository;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    public function __construct(CouponRepository $couponRepo, UserRepository $userRepo)
+    {
+        $this->couponRepository = $couponRepo;
+        $this->userRepository = $userRepo;
+    }
+
+    /**
+     * Display a listing of the Coupon.
+     * GET|HEAD /coupons
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        try{
+            $this->couponRepository->pushCriteria(new RequestCriteria($request));
+            $this->couponRepository->pushCriteria(new LimitOffsetCriteria($request));
+            $this->couponRepository->pushCriteria(new ValidCriteria());
+        } catch (RepositoryException $e) {
+            return $this->sendError($e->getMessage());
+        }
+
+        $coupon_code = substr($request->search, 5);
+        $customer_id = auth()->id();
+        $history = CouponHistory::where('coupon_code', $coupon_code)
+                    ->where('customer_id', $customer_id)
+                    ->get();
+        
+        if (count($history) > 0) {
+            $customer = $this->userRepository->findWithoutFail($customer_id);
+            if (!empty($customer)) {
+                Notification::send([$customer], new CouponUsed($coupon_code));
+            }
+            return $this->sendResponse([], 'Coupons retrieved successfully');
+        } else {
+            $coupons = $this->couponRepository->all();
+            return $this->sendResponse($coupons->toArray(), 'Coupons retrieved successfully');
+        }
+    }
+
+    /**
+     * Display the specified Coupon.
+     * GET|HEAD /coupons/{id}
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        /** @var Coupon $coupon */
+        if (!empty($this->couponRepository)) {
+            $coupon = $this->couponRepository->findWithoutFail($id);
+        }
+
+        if (empty($coupon)) {
+            return $this->sendError('Coupon not found');
+        }
+
+        return $this->sendResponse($coupon->toArray(), 'Coupon retrieved successfully');
+    }
+}
---
+++ app/Http/Controllers/BroadCastController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use LaravelFCM\Message\Topics;
+use FCM;
+class BroadCastController extends Controller
+{
+    /** @var  UserRepository */
+    private $userRepository;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(UserRepository $userRepo, Client $client)
+    {
+        parent::__construct();
+        $this->userRepository = $userRepo;
+        $this->client = $client;
+    }
+
+    public function index()
+    {
+        return view('broadcast.index');
+    }
+
+    public function send(Request $request)
+    {
+        $message = $request->input('message');
+        /*
+        if ($message != '') {
+            $groupName = "DeliapDeviceGroup";
+            $tokens = explode(',', setting('firebase_device_tokens'));
+            $tokenCnt = ceil(count($tokens) / 100);
+            $notifKeys = explode(',', setting('firebase_notification_group_key'));
+            if($tokenCnt != 1 || $tokens[0] != '') {
+                for($i = 0; $i < $tokenCnt; $i++) {
+                    $tempTokens = array_slice($tokens, $i * 100, 100);
+                    $tmpTokens = array_values(array_filter($tempTokens, function($el) {
+                        return $el != '';
+                    }));
+
+                    $tempGroupName = $groupName . $i;
+                    $response = $this->client->post('https://fcm.googleapis.com/fcm/notification', [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'Authorization' => 'key=' . config('fcm.server.key'),
+                            'project_id' => config('fcm.sender.id')
+                        ],
+                        'body' => json_encode([
+                            'operation' => 'remove',
+                            'notification_key_name' => $tempGroupName,
+                            'notification_key' => $notifKeys[$i],
+                            'registration_ids' => $tmpTokens
+                        ])
+                    ]);
+                }
+            }
+
+            $newTokens = $this->userRepository->all()->pluck('device_token')->toArray();
+            $newToken_str = implode(',', $newTokens);
+            $newTokenCnt = ceil(count($newTokens) / 100);
+            $newNotifKeys = [];
+            for($i = 0 ; $i < $newTokenCnt; $i++) {
+                $tempTokens = array_slice($newTokens, $i * 100, 100);
+                $tmpTokens = array_values(array_filter($tempTokens, function($el) {
+                    return $el != '';
+                }));
+                $tempGroupName = $groupName . $i;
+                $response = $this->client->post('https://fcm.googleapis.com/fcm/notification', [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'key=' . config('fcm.server.key'),
+                        'project_id' => config('fcm.sender.id')
+                    ],
+                    'body' => json_encode([
+                        'operation' => 'create',
+                        'notification_key_name' => $tempGroupName,
+                        'registration_ids' => $tmpTokens
+                    ])
+                ]);
+
+                $resObj = json_decode($response->getBody());
+                $newNotifKeys[$i] = $resObj->notification_key;
+
+                $ttt = $resObj->notification_key;
+                print($ttt);
+                $response = $this->client->post('https://fcm.googleapis.com/fcm/send', [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'key=' . config('fcm.server.key')
+                    ],
+                    'body' => json_encode([
+                        "to" => $ttt,
+                        "notification" => [
+                            "title" => $message,
+                            "text" => "",
+                            "sound" => "default"
+                        ],
+                        "priority" => "high"
+                    ])
+                ]);
+            }
+            $notifKeys_str = implode(',', $newNotifKeys);
+            $input['firebase_device_tokens'] = $newToken_str;
+            $input['firebase_notification_group_key'] = $notifKeys_str;
+            setting($input)->save();
+        } */
+        $notificationBuilder = new PayloadNotificationBuilder($message);
+        $notificationBuilder->setBody('')
+                            ->setSound('default');
+
+        $notification = $notificationBuilder->build();
+
+        $topic = new Topics();
+        $topic->topic('broadCastMessage');
+
+        $topicResponse = FCM::sendToTopic($topic, null, $notification, null);
+        return redirect()->route('broadcastMessage.index');
+    }
+}
